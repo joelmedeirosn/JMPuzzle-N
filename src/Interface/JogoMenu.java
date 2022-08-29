d856af80069d5ef7acec5bfeab8de69dc7d7a89a
Marcos André de Barros Prudêncio <104862602+MarcosABP@users.noreply.github.com>
2022-08-28 23:17:59 -0300

    Add files via upload

---
+++ src/Interface/JogoMenu.java
@@ -0,0 +1,34 @@
+package Interface;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class JogoMenu extends JFrame {
+
+    private JPanel Menu;
+    private JLabel Title;
+    private JButton iniciarButton;
+    private JButton tipoButton;
+    private JButton dificuldadeButton;
+    private JButton sairButton;
+
+    public JogoMenu(String title){
+        super(title);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setContentPane(Menu);
+
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new JogoMenu("jogoPuzzle");
+        frame.setSize(new Dimension(500,500));
+        frame.setLocationRelativeTo(null);
+
+
+
+        frame.setVisible(true);
+
+
+    }
+
+}
