ec170c8aeea8dd738a101d20d02c5a160ed82c23
Marcos André de Barros Prudêncio <104862602+MarcosABP@users.noreply.github.com>
2022-09-08 16:50:29 -0300

    Add files via upload

--- src/PuzzleN/Main.java
+++ src/PuzzleN/Main.java
@@ -10,7 +10,7 @@ public class Main {
         System.out.println("Digite seu nome:");
         x = input.nextLine();
         Puzzle.setNomeUsuario(x);
-        System.out.println("Tamanho jogo mano");
+        System.out.println("Tamanho do jogo ");
         y = input.nextInt();
         Puzzle jogo1 = new Puzzle();
         jogo1.NiveldoPuzzle(y);
