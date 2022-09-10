7babead379b8876e8c60b6d8be3fae102d6362f6
Marcos André de Barros Prudêncio <104862602+MarcosABP@users.noreply.github.com>
2022-09-10 11:40:48 -0300

    Add files via upload

--- src/Interface/Jogo.java
+++ src/Interface/Jogo.java
@@ -3,27 +3,16 @@ package Interface;
 import javax.swing.*;
 import java.awt.*;
 
-public class Jogo extends JFrame{
+public class Jogo extends JPanel{
 
     private JPanel Jogo;
 
-    public Jogo(String title){
-        super(title);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setContentPane(Jogo);
-
+    public Jogo(){
+        Iniciar();
     }
-
-    public static void main(String[] args) {
-        JFrame frame = new Jogo("jogoPuzzle");
-        frame.setSize(new Dimension(500,500));
-        frame.setLocationRelativeTo(null);
-        frame.setResizable(false);
-
-
-        frame.setVisible(true);
-
-
+    public void Iniciar(){
+        setSize(new Dimension(500,500));
+        setVisible(true);
     }
 }
 
--- src/Interface/JogoMenu.java
+++ src/Interface/JogoMenu.java
@@ -2,8 +2,10 @@ package Interface;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class JogoMenu extends JFrame {
+public class JogoMenu extends JFrame implements ActionListener {
 
     private JPanel Menu;
     private JLabel Title;
@@ -12,23 +14,44 @@ public class JogoMenu extends JFrame {
     private JButton dificuldadeButton;
     private JButton sairButton;
 
-    public JogoMenu(String title){
-        super(title);
+    public JogoMenu(){
+
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setContentPane(Menu);
+
+        iniciarButton.addActionListener(this);
+        tipoButton.addActionListener(this);
+        dificuldadeButton.addActionListener(this);
+        sairButton.addActionListener(this);
+
 
     }
 
     public static void main(String[] args) {
-        JFrame frame = new JogoMenu("menuPuzzle");
+        JFrame frame = new JogoMenu();
         frame.setSize(new Dimension(500,500));
         frame.setLocationRelativeTo(null);
         frame.setResizable(false);
-
-
         frame.setVisible(true);
 
 
     }
 
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if(e.getSource()==iniciarButton){
+            Jogo iniciar = new Jogo();
+            setContentPane(iniciar);
+            iniciar.revalidate();
+        }
+        if(e.getSource()==tipoButton){
+            System.out.println("bb");
+        }
+        if(e.getSource()==dificuldadeButton){
+
+        }
+        if(e.getSource()==sairButton){
+            System.exit(0);
+        }
+    }
 }
