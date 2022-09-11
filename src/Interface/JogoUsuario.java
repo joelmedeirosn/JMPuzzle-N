891a15ec2ea1cc5b50360edc768540b0171274aa
MarcosABP <prudenciomarcos10@gmail.com>
2022-09-11 19:01:17 -0300

    interface funfando

---
+++ src/Interface/JogoUsuario.java
@@ -0,0 +1,22 @@
+package Interface;
+
+public class JogoUsuario {
+
+    private int nivel;
+    private String tipo;
+    private String usuario;
+
+
+
+    public int getNivel() {return (nivel);}
+
+    public void setNivel(int nivel){this.nivel = nivel;}
+
+    public String getTipo() {return (tipo);}
+
+    public void setTipo(String tipo) {this.tipo = tipo;}
+
+    public void setNomeUsuario(String usuario){this.usuario = usuario;}
+
+    public String getNomeUsuario(){return usuario;}
+}
